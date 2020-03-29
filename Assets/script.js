377f6719192b9a79e2c24df7f4515737382ebc35
Marc Strong <marcstrong@yahoo.com>
2020-03-29 18:57:27 -0500

    move eventBlock object again, fix render function again

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
-    
+  
+  var eventBlock = {
+    block:  [{
+      hour: "9",
+      activity: "",
+    }, {
+      hour: "10",
+      activity: "",
+    }, {
+      hour: "11",
+      activity: "",
+    }, {
+      hour: "12",
+      activity: "",
+    }, {
+      hour: "13",
+      activity: "",
+    }, {
+      hour: "14",
+      activity: "",
+    }, {
+      hour: "15",
+      activity: "",
+    }, {
+      hour: "16",
+      activity: "",
+    }, {
+      hour: "17",
+      activity: "",
+    }]
+  }         
+
   // Add Today's date onto top of daily planner
   console.log(moment().format('LL'))
     var today = moment().format('LL');
@@ -8,38 +39,36 @@ $(document).ready(function() {
     //get locale storage
   function render(Hour) {
     var get = localStorage.getItem('saved')
-    eventBlock = JSON.parse(get)
-    
-      console.log(eventBlock.block)
-      console.log(eventBlock.block[0].activity)
-    
-      if (eventBlock.block[0].hour == 9) {
-        writeText (eventBlock.block[0].activity)
-      }
-      if (eventBlock.block[1].hour == 10) {
-        writeText1 (eventBlock.block[1].activity)
-      }
-      if (eventBlock.block[2].hour == 11) {
-        writeText2 (eventBlock.block[2].activity)
-      }
-      if (eventBlock.block[3].hour == 12) {
-        writeText3 (eventBlock.block[3].activity)
-      }
-      if (eventBlock.block[4].hour == 13) {
-        writeText4 (eventBlock.block[4].activity)
-      }
-      if (eventBlock.block[5].hour == 14) {
-        writeText5 (eventBlock.block[5].activity)
-      }
-      if (eventBlock.block[6].hour == 15) {
-        writeText6 (eventBlock.block[6].activity)
-      }
-      if (eventBlock.block[7].hour == 16) {
-        writeText7 (eventBlock.block[7].activity)
-      }
-      if (eventBlock.block[8].hour == 17) {
-        writeText8 (eventBlock.block[8].activity)
-      }
+    if (get){
+      eventBlock = JSON.parse(get)
+    }
+    if (eventBlock.block[0].hour == 9) {
+      writeText (eventBlock.block[0].activity)
+    }
+    if (eventBlock.block[1].hour == 10) {
+      writeText1 (eventBlock.block[1].activity)
+    }
+    if (eventBlock.block[2].hour == 11) {
+      writeText2 (eventBlock.block[2].activity)
+    }
+    if (eventBlock.block[3].hour == 12) {
+      writeText3 (eventBlock.block[3].activity)
+    }
+    if (eventBlock.block[4].hour == 13) {
+      writeText4 (eventBlock.block[4].activity)
+    }
+    if (eventBlock.block[5].hour == 14) {
+      writeText5 (eventBlock.block[5].activity)
+    }
+    if (eventBlock.block[6].hour == 15) {
+      writeText6 (eventBlock.block[6].activity)
+    }
+    if (eventBlock.block[7].hour == 16) {
+      writeText7 (eventBlock.block[7].activity)
+    }
+    if (eventBlock.block[8].hour == 17) {
+      writeText8 (eventBlock.block[8].activity)
+    }
   }
 
   //https://stackoverflow.com/questions/26346956/inserting-text-into-textarea-with-javascript-for-google-chrome-extension
@@ -74,12 +103,12 @@ $(document).ready(function() {
   }
 
   // figure how moment.js works, define current time
-    var now = moment();
-    console.log(now)
-    console.log(now._locale)
-    console.log(now._locale._calendar)
-    var now1 = moment("1995-12-25");
-    console.log(now1)
+  var now = moment();
+  // console.log(now)
+  // console.log(now._locale)
+  // console.log(now._locale._calendar)
+  var now1 = moment("1995-12-25");
+  // console.log(now1)
   // $("#currentDay").text(now1.format('LL'))
 
   
@@ -88,7 +117,7 @@ $(document).ready(function() {
     render(firstHour)
     var time9AM = moment({hour: firstHour});
     // $("#currentDay").text(event9AM.format('LT'))
-    console.log(now.diff(time9AM, 'hours', 'minutes'))
+    // console.log(now.diff(time9AM, 'hours', 'minutes'))
     // localStorage.getItem('myJSON')
     // alert (myJSON)
     if (now.diff(time9AM, 'hours', 'minutes') > 1 ){
@@ -105,7 +134,7 @@ $(document).ready(function() {
     render(firstHour+1)
     var time10AM = moment({hour: firstHour+1});
     // $("#currentDay").text(event9AM.format('LT'))
-    console.log(now.diff(time10AM, 'hours', 'minutes'))
+    // console.log(now.diff(time10AM, 'hours', 'minutes'))
     if (now.diff(time10AM, 'hours', 'minutes') > 1 ){
       $("#10AMevent").addClass("past");
     }
@@ -119,7 +148,7 @@ $(document).ready(function() {
     //color 11am timeblock
     var time11AM = moment({hour: firstHour+2});
     // $("#currentDay").text(event9AM.format('LT'))
-    console.log(now.diff(time11AM, 'hours', 'minutes'))
+    // console.log(now.diff(time11AM, 'hours', 'minutes'))
     if (now.diff(time11AM, 'hours', 'minutes') > 1 ){
       $("#11AMevent").addClass("past");
     }
@@ -130,7 +159,6 @@ $(document).ready(function() {
       $("#11AMevent").addClass("future");
     }
 
-    
     //color 12pm timeblock
     var time12PM = moment({hour: firstHour+3});
     // $("#currentDay").text(event9AM.format('LT'))
@@ -199,7 +227,7 @@ $(document).ready(function() {
     //color 5pm timeblock
     var time5PM = moment({hour: firstHour+8});
     // $("#currentDay").text(event9AM.format('LT'))
-    console.log(now.diff(time5PM, 'hours', 'minutes'))
+    // console.log(now.diff(time5PM, 'hours', 'minutes'))
     if (now.diff(time5PM, 'hours', 'minutes') > 1 ){
       $("#5PMevent").addClass("past");
     }
@@ -210,137 +238,103 @@ $(document).ready(function() {
       $("#5PMevent").addClass("future");
     }
 
-    
-    // $("#btn9AM").on("click", function(e){
-    var eventBlock = {
-          block:  [{
-            hour: "9",
-            activity: "",
-          }, {
-            hour: "10",
-            activity: "",
-          }, {
-            hour: "11",
-            activity: "",
-          }, {
-            hour: "12",
-            activity: "",
-          }, {
-            hour: "13",
-            activity: "",
-          }, {
-            hour: "14",
-            activity: "",
-          }, {
-            hour: "15",
-            activity: "",
-          }, {
-            hour: "16",
-            activity: "",
-          }, {
-            hour: "17",
-            activity: "",
-          }]
-        }         
-  //  console.log(eventBlock.block[1])
-
     //Save on 9AM button
-      document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    document.getElementsByTagName("button")[0].addEventListener("click", function() {
+    
+      var nine = document.getElementById('activity').value;
+      eventBlock.block[0].activity = nine 
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity').innerHTML = nine;
+      console.log(eventBlock.block)
+    })
       
-        var nine = document.getElementById('activity').value;
-        eventBlock.block[0].activity = nine 
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity').innerHTML = nine;
-        console.log(eventBlock.block)
-      })
-      
-      //Save on 10AM button
-      document.getElementsByTagName("button")[1].addEventListener("click", function (){
-        var ten = document.getElementById('activity1').value;
-        eventBlock.block[1].activity = ten
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity1').innerHTML = ten;
-        console.log(eventBlock.block)
-      })
+    //Save on 10AM button
+    document.getElementsByTagName("button")[1].addEventListener("click", function (){
+      var ten = document.getElementById('activity1').value;
+      eventBlock.block[1].activity = ten
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity1').innerHTML = ten;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 11AM button
-      document.getElementsByTagName("button")[2].addEventListener("click", function (){
-        var eleven = document.getElementById('activity2').value;
-        eventBlock.block[2].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity2').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 11AM button
+    document.getElementsByTagName("button")[2].addEventListener("click", function (){
+      var eleven = document.getElementById('activity2').value;
+      eventBlock.block[2].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity2').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 12PM button
-      document.getElementsByTagName("button")[3].addEventListener("click", function (){
-        var eleven = document.getElementById('activity3').value;
-        eventBlock.block[3].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity3').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 12PM button
+    document.getElementsByTagName("button")[3].addEventListener("click", function (){
+      var eleven = document.getElementById('activity3').value;
+      eventBlock.block[3].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity3').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 1PM button
-      document.getElementsByTagName("button")[4].addEventListener("click", function (){
-        var eleven = document.getElementById('activity4').value;
-        eventBlock.block[4].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity4').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 1PM button
+    document.getElementsByTagName("button")[4].addEventListener("click", function (){
+      var eleven = document.getElementById('activity4').value;
+      eventBlock.block[4].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity4').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 2PM button
-      document.getElementsByTagName("button")[5].addEventListener("click", function (){
-        var eleven = document.getElementById('activity5').value;
-        eventBlock.block[5].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity5').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 2PM button
+    document.getElementsByTagName("button")[5].addEventListener("click", function (){
+      var eleven = document.getElementById('activity5').value;
+      eventBlock.block[5].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity5').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 3PM button
-      document.getElementsByTagName("button")[6].addEventListener("click", function (){
-        var eleven = document.getElementById('activity6').value;
-        eventBlock.block[6].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity6').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 3PM button
+    document.getElementsByTagName("button")[6].addEventListener("click", function (){
+      var eleven = document.getElementById('activity6').value;
+      eventBlock.block[6].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity6').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 4PM button
-      document.getElementsByTagName("button")[7].addEventListener("click", function (){
-        var eleven = document.getElementById('activity7').value;
-        eventBlock.block[7].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity7').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 4PM button
+    document.getElementsByTagName("button")[7].addEventListener("click", function (){
+      var eleven = document.getElementById('activity7').value;
+      eventBlock.block[7].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity7').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
 
-      //Save on 5PM button
-      document.getElementsByTagName("button")[8].addEventListener("click", function (){
-        var eleven = document.getElementById('activity8').value;
-        eventBlock.block[8].activity = eleven
-        var saved = JSON.stringify(eventBlock)
-        // alert (saved)
-        localStorage.setItem('saved', saved)
-        document.getElementById('activity8').innerHTML = eleven;
-        console.log(eventBlock.block)
-      })
+    //Save on 5PM button
+    document.getElementsByTagName("button")[8].addEventListener("click", function (){
+      var eleven = document.getElementById('activity8').value;
+      eventBlock.block[8].activity = eleven
+      var saved = JSON.stringify(eventBlock)
+      // alert (saved)
+      localStorage.setItem('saved', saved)
+      document.getElementById('activity8').innerHTML = eleven;
+      console.log(eventBlock.block)
+    })
   });
